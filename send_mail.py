13c78ba316971940949ab6f0e2ba9d0350507da1
Eduardo Lizama Nunez <elizamanu@icloud.com>
2024-03-11 21:49:52 -0300

    modificaciones

--- send_mail.py
+++ send_mail.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_mail import Mail, Message
 import os
 
@@ -14,41 +14,27 @@ app.config['MAIL_DEFAULT_SENDER'] = ('SINTEG', 'sinteg@efesur.cl')
 
 mail = Mail(app)
 
+@app.route('/')
+def index():
+    return "Hello, world"
+
 @app.route('/enviar_correo', methods=['GET'])
 def enviar_correo():
     try:
         # Obtener datos del request
         # data = request.json
-        destinatarios = ['elizama@alumnos.ubiobio.cl','eduardo.lizama@efesur.cl']
+        destinatarios = ['elizama@alumnos.ubiobio.cl']
         asunto = 'correo de prueba'
-        cuerpo = f'<html><head><style>{get_css_styles()}</style></head><body><b>esto es un correo de prueba</b></body><br><br></html>'
-        archivos_adjuntos = ['C:/Users/Eduardo/Desktop/send_mail/archivo_prueba.txt']
-
+        
         # Enviar correo electrónico
-        message = Message(subject=asunto, bcc=destinatarios, html=cuerpo)
-
-        for ruta_archivo in archivos_adjuntos:
-            nombre_archivo = os.path.basename(ruta_archivo)
-            with app.open_resource(ruta_archivo) as adjunto:
-                message.attach(filename=nombre_archivo, content_type='application/octet-stream', data=adjunto.read())
-
+        message = Message(subject=asunto, bcc=destinatarios)
+        message.html = render_template('mail.html', username='Eduardo')
+       
         mail.send(message)
-
         return jsonify({'mensaje': 'Correo enviado'})
+        #return render_template('mail.html', username='Eduardo')
     except Exception as e:
         return jsonify({'error': f'Error al enviar el correo: {str(e)}'})
     
-def get_css_styles():
-    # Define aquí tus estilos CSS
-    css_styles = """
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            color: #333;
-        }
-        /* Agrega más estilos según sea necesario */
-    """
-    return css_styles
-
 if __name__ == '__main__':
     app.run(debug=True)
