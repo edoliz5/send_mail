b5d6b0c70cec55a5afdf47a27f5672de13aa384f
Eduardo Lizama Nunez <elizamanu@icloud.com>
2024-03-12 00:23:28 -0300

    fix

--- send_mail.py
+++ send_mail.py
@@ -11,6 +11,7 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'sinteg@efesur.cl'
 app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_DEFAULT_SENDER'] = ('SINTEG', 'sinteg@efesur.cl') 
+app.config['MAIL_SUPPRESS_SEND'] = False
 
 mail = Mail(app)
 
