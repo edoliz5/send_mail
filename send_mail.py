1dc8c2c0c6173c73386abf478a9fc7889de5f793
Eduardo Lizama Nunez <elizamanu@icloud.com>
2024-03-11 23:12:04 -0300

    prints

--- send_mail.py
+++ send_mail.py
@@ -9,7 +9,7 @@ app.config['MAIL_SERVER'] = 'smtp.office365.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'sinteg@efesur.cl'
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_PASSWORD'] = 'Efesur..2023#'
 app.config['MAIL_DEFAULT_SENDER'] = ('SINTEG', 'sinteg@efesur.cl') 
 
 mail = Mail(app)
@@ -25,15 +25,19 @@ def enviar_correo():
         # data = request.json
         destinatarios = ['elizama@alumnos.ubiobio.cl']
         asunto = 'correo de prueba'
+        print("\nSeteo variables destinatarios y asunto")
         
         # Enviar correo electrónico
         message = Message(subject=asunto, bcc=destinatarios)
         message.html = render_template('mail.html', username='Eduardo')
+        print("\ncrea y setena variblae mensaje")
        
         mail.send(message)
+        print("envia maile")
         return jsonify({'mensaje': 'Correo enviado'})
         #return render_template('mail.html', username='Eduardo')
     except Exception as e:
+        print("error")
         return jsonify({'error': f'Error al enviar el correo: {str(e)}'})
     
 if __name__=="__main__":
