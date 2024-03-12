913f83380178050082d7d27df2550f77dfd192d8
Eduardo Lizama Nunez <elizamanu@icloud.com>
2024-03-12 00:51:18 -0300

    prueba

--- send_mail.py
+++ send_mail.py
@@ -14,6 +14,13 @@ app.config['MAIL_DEFAULT_SENDER'] = ('SINTEG', 'sinteg@efesur.cl')
 app.config['MAIL_SUPPRESS_SEND'] = False
 app.config['TESTING'] = False
 
+app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = '0bf6ffad235de9'
+app.config['MAIL_PASSWORD'] = 'c504c3871be3e9'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
 mail = Mail(app)
 
 @app.route('/')
